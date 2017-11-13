94654128bda51dac2b13a2c438dfe27b3899a1f7
Carlos <carlosmonteroreina@gmail.com>
2017-11-13 18:33:52 +0100

    Minimo 1 EA final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Http, Response} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  student = {};
+  students = [];
+  subject = {};
+  subjects = [];
+
+  constructor(private http: Http) {
+    this.student = {
+      'name': '',
+      'address': '',
+      'phone': ''
+    };
+    this.getStudents();
+  }
+
+  getStudents() {
+    this.http.get('http://localhost:3000/api/students/').map((res: Response) => res.json()).subscribe(data => {
+      this.students = data;
+    });
+  }
+
+  onSubmit() {
+    console.log("submit");
+    this.http.post('http://localhost:3000/api/students/', this.student).subscribe();
+
+  }
+
+
+
+
+
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {HttpModule} from "@angular/http";
+//import {Routes, RouterModule} from "@angular/router";
+//import {ListComponent} from "./list/list.component";
+import 'rxjs/add/operator/map';
+
+/*
+const appRoutes: Routes = [
+  {path: 'list', component: ListComponent}
+]*/
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    //RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
